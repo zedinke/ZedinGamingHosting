26a2f6b0bd8ea64af6b3b3a47d270d45dbce5825
zedinke <geleako@gmail.com>
2025-11-26 13:57:27 +0100

    Fix: Hozzaadva egyszeru generateBuildId fuggveny Next.js 14.0.4 bug javitasahoz

--- next.config.js
+++ next.config.js
@@ -26,6 +26,11 @@ const nextConfig = {
   },
   // Server Actions are available by default in Next.js 14+
   output: 'standalone', // Docker optimalizáláshoz
+  // Fix for Next.js 14.0.4 build ID generation bug
+  generateBuildId: () => {
+    // Return a simple build ID - Next.js will use this instead of trying to call generate()
+    return `build-${Date.now()}`;
+  },
   webpack: (config, { isServer }) => {
     // Exclude optional dependencies from build
     if (isServer) {
