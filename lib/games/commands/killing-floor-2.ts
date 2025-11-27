0c3fd81920dc4030355381bb4d1bd142611ab98a
zedinke <geleako@gmail.com>
2025-11-27 21:13:25 +0100

    HozzĂˇadĂˇs: Killing Floor 2 commands fĂˇjl
    - killing-floor-2.ts commands fĂˇjl lĂ©trehozva
    - startCommand Ă©s stopCommand definiĂˇlva

---
+++ lib/games/commands/killing-floor-2.ts
@@ -0,0 +1,9 @@
+/**
+ * Killing Floor 2 indító és leállító parancsok
+ */
+
+export const commands = {
+  startCommand: './Binaries/Linux/KFGameServer KF-BurningParis?Game=KFGameContent.KFGameInfo_Endless?Difficulty=2?MaxPlayers=6 -Port=7707 -QueryPort=27015',
+  stopCommand: 'quit',
+};
+
