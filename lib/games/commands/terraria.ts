e058a3451b6c2f89ccb1612069a5f0eb6cec3d90
zedinke <geleako@gmail.com>
2025-11-27 21:43:13 +0100

    HozzĂˇadĂˇs: Terraria commands fĂˇjl

---
+++ lib/games/commands/terraria.ts
@@ -0,0 +1,9 @@
+/**
+ * Terraria indító és leállító parancsok
+ */
+
+export const commands = {
+  startCommand: './TerrariaServer.bin.x86_64 -port {port} -world {world} -maxplayers {maxPlayers}',
+  stopCommand: 'exit',
+};
+
