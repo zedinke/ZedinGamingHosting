6e8132543db96231621ec6194d90db1f3a836a71
zedinke <geleako@gmail.com>
2025-12-04 23:07:06 +0100

    fix: Use Wine wrapper for ARK Ascended Windows binary on Linux

--- lib/games/commands/ark-ascended.ts
+++ lib/games/commands/ark-ascended.ts
@@ -1,9 +1,14 @@
 /**
  * ARK: Survival Ascended indító és leállító parancsok
+ * 
+ * MEGJEGYZÉS: Az ARK Survival Ascended csak Windows-os verzióban létezik!
+ * Linux alatt Wine-t használunk a Windows szerverek futtatásához.
  */
 
 export const commands = {
-  startCommand: './ShooterGame/Binaries/Linux/ShooterGameServer TheIsland_WP?listen?SessionName="{name}"?Port={port}?QueryPort={queryPort}?ServerPassword=?AdminPassword={adminPassword}?MaxPlayers={maxPlayers} -server -log',
+  // ARK Ascended: Wine-t használunk a Windows bináris futtatásához
+  // ShooterGameServer.exe: Windows szerver executable
+  startCommand: 'wine ./ShooterGame/Binaries/Win64/ShooterGameServer.exe TheIsland_WP?listen?SessionName="{name}"?Port={port}?QueryPort={queryPort}?ServerPassword=?AdminPassword={adminPassword}?MaxPlayers={maxPlayers} -server -log',
   stopCommand: 'quit',
 };
 
